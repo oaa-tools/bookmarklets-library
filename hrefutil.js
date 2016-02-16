0ced83a97585ec5718efad8ddc13e0ebb032f776
Nicholas Hoyt <nhoyt@illinois.edu>
2016-02-16 12:21:56 -0600

    Add url param in place of baseURI

--- hrefutil.js
+++ hrefutil.js
@@ -1,11 +1,8 @@
-function createBookmarkletHref (name) {
+function createBookmarkletHref (name, url) {
   var bookmarklet = "window.a11y" + name;
   var scriptname = name.toLowerCase() + '.js';
+  var baseUrl = url.trim();
+  if (!baseUrl.endsWith('/')) baseUrl += '/';
 
-  var baseURI = document.baseURI;
-  var domain = baseURI.split('/')[2];
-  if (domain === 'accessibility-bookmarklets.org' || domain === 'oaa-tools.github.io' || domain === 'nhoyt.com') {
-    baseURI = baseURI.replace(/http:/, 'https:');
-  }
-  return "javascript:if(" + bookmarklet + ")" + bookmarklet + ".run();else{(function(){var%20link=document.createElement('link');link.rel='stylesheet';link.type='text/css';link.href='" + baseURI + "build/styles.css';document.getElementsByTagName('head')[0].appendChild(link);var%20script=document.createElement('script');script.type='text/javascript';script.src='" + baseURI + "build/" + scriptname + "';document.getElementsByTagName('head')[0].appendChild(script);})();}";
+  return "javascript:if(" + bookmarklet + ")" + bookmarklet + ".run();else{(function(){var%20link=document.createElement('link');link.rel='stylesheet';link.type='text/css';link.href='" + baseUrl + "build/styles.css';document.getElementsByTagName('head')[0].appendChild(link);var%20script=document.createElement('script');script.type='text/javascript';script.src='" + baseUrl + "build/" + scriptname + "';document.getElementsByTagName('head')[0].appendChild(script);})();}";
 }
