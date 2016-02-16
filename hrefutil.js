1063d7d5647a15b0352240a2ce2829ea5d6474e5
Nicholas Hoyt <nhoyt@illinois.edu>
2016-02-16 09:16:11 -0600

    Fix protocol string replace

--- hrefutil.js
+++ hrefutil.js
@@ -3,11 +3,9 @@ function createBookmarkletHref (name) {
   var scriptname = name.toLowerCase() + '.js';
 
   var baseURI = document.baseURI;
-  var baseURIArray = baseURI.split('/');
-  var protocol = baseURIArray[0];
-  var domain = baseURIArray[2];
+  var domain = baseURI.split('/')[2];
   if (domain === 'accessibility-bookmarklets.org' || domain === 'oaa-tools.github.io' || domain === 'nhoyt.com') {
-    if (protocol === 'http') baseURI = baseURI.replace(/http/, 'https');
+    baseURI = baseURI.replace(/http:/, 'https:');
   }
   return "javascript:if(" + bookmarklet + ")" + bookmarklet + ".run();else{(function(){var%20link=document.createElement('link');link.rel='stylesheet';link.type='text/css';link.href='" + baseURI + "build/styles.css';document.getElementsByTagName('head')[0].appendChild(link);var%20script=document.createElement('script');script.type='text/javascript';script.src='" + baseURI + "build/" + scriptname + "';document.getElementsByTagName('head')[0].appendChild(script);})();}";
 }
